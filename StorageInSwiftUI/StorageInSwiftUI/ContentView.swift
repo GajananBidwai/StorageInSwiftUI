dc12ab8c969b728b22c45cb028e4f6e16d404f49
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-09 10:12:45 +0530

    Initial commit

---
+++ StorageInSwiftUI/StorageInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StorageInSwiftUI
+//
+//  Created by Neosoft on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StorageInSwiftUI/StorageInSwiftUI/StorageInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageInSwiftUIApp.swift
+//  StorageInSwiftUI
+//
+//  Created by Neosoft on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StorageInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
