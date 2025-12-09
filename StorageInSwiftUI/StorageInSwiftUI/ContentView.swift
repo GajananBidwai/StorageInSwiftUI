3b2993bada167b0e8468e46b19236280bec1c35d
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-09 10:52:36 +0530

    @AppStorage

--- StorageInSwiftUI/StorageInSwiftUI/ContentView.swift
+++ StorageInSwiftUI/StorageInSwiftUI/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    //AppStorage(String)- This property wrappers stores and retrives the values from user default The argument is a string with the key of the value we want to access.
+    @AppStorage("StoreCounter") var counter: Int = 0
+    
+    @AppStorage("interval") var interval = Date.timeIntervalSinceReferenceDate
+    @AppStorage("message") var message: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        HStack {
+//            //Value is preseisted even thoungh the app is shut down and restarted
+//            Stepper("", value: $counter)
+//                .labelsHidden()
+//            Text("\(counter)")
+//                .font(.title)
+//        }
+        
+//      It persist the time when user last close the app
+        HStack {
+            Text("\(message)")
         }
-        .padding()
+        .onAppear {
+            let calender = Calendar.current
+            let lastDate = Date(timeIntervalSinceReferenceDate: interval)
+            let componant = calender.dateComponents([.day, .dayOfYear, .hour, .minute, .month, .second, .nanosecond], from: lastDate, to: Date())
+            message = "Time\(componant)"
+            interval = Date.timeIntervalSinceReferenceDate
+        }
+        
     }
 }
 
