acc5fcd4166f5d0337d54e625cf66aaa86441c8f
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-09 11:52:10 +0530

    Container or model container

--- StorageInSwiftUI/StorageInSwiftUI/StorageInSwiftUIApp.swift
+++ StorageInSwiftUI/StorageInSwiftUI/StorageInSwiftUIApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct StorageInSwiftUIApp: App {
+    
+    @State private var product = Product(id: 0, title: "", price: 0, category: "", image: "", rating: Rating(rate: 0, count: 0))
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                //Container should tells the compiler that we can able to get a access to every view
+                .environment(product)
+                .modelContainer(for: [Product.self])
+            
         }
     }
 }
