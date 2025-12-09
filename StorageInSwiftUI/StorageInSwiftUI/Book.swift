523fae1b651a09891ac739551cb4faaddfa2f4a0
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-09 15:26:09 +0530

    @Attribute

--- StorageInSwiftUI/StorageInSwiftUI/Book.swift
+++ StorageInSwiftUI/StorageInSwiftUI/Book.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftData
 
 //@Model- macro uses a class to define the structure of the data our app can store
+//@Attribute is used inside your @Model class to add extra rules to a property. It helps SwiftData understand how this property should be stored.
 
 @Model
 class Book: Identifiable {
